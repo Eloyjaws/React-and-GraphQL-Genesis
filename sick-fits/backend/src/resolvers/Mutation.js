e20007845096de311ec54f92411e8cb335e73dff
eajiboye <eajiboye@inlaks.com>
2018-11-23 23:45:50 +0100

    Setup Apollo client

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+const mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO check if they are logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     const newDog = {name: args.name};
+    //     global.dogs = [...global.dogs, newDog];
+    //     console.log(args);
+    //     return newDog;
+    // }
+};
 
 module.exports = mutations;
